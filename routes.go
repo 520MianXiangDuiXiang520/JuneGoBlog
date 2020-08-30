74043ffbe6b12833591855757e37b5cd7bc6f0b5
520MianXiangDuiXiang520 <15364968962@163.com>
2020-08-30 18:17:55 +0800

    :sparkles: add: 添加 CORS 中间件处理 OPTIONS

--- routes.go
+++ routes.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"JuneGoBlog/src/junebao.top"
+	middleware2 "JuneGoBlog/src/junebao.top/middleware"
 	"JuneGoBlog/src/middleware"
 	"JuneGoBlog/src/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func Register(c *gin.Engine) {
-	c.Use(middleware.ApiView())
+	c.Use(middleware.ApiView(), middleware2.CorsHandler())
 	junebao_top.URLPatterns(c, "api/article", routes.ArticleRegister)
 	junebao_top.URLPatterns(c, "api/tag", routes.TagRegister)
 	junebao_top.URLPatterns(c, "api/talking", routes.TalkingRegister)
