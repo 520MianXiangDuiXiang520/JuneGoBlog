f07ac9edad8a1c9dde34aeeab989d3c842086933
520MianXiangDuiXiang520 <15364968962@163.com>
2020-10-03 16:38:02 +0800

    :bug: 上传认证中间件

---
+++ src/middleware/auth.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"JuneGoBlog/src/dao"
+	"JuneGoBlog/src/junebao.top/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+// 使用Token进行认证，如果使用此中间件后请求中 Cookie 未携带 SESSIONID 字段
+// 或所携带的值错误，将会返回一个 401 的 HTTP 错误，用于后台接口认证
+func TokenAuth(context *gin.Context) (middleware.UserBase, bool) {
+	token, err := context.Cookie("SESSIONID")
+	if err != nil {
+		return nil, false
+	}
+	user, ok := dao.GetUserByToken(token)
+	if !ok {
+		return nil, false
+	}
+	return user, true
+}
+
+// 用于发表评论时的认证，不会拦截未登录的请求
+func TalkingAuth(ctx *gin.Context) (middleware.UserBase, bool) {
+	token, err := ctx.Cookie("SESSIONID")
+	if err != nil {
+		return nil, true
+	}
+	user, _ := dao.GetUserByToken(token)
+	return user, true
+}
