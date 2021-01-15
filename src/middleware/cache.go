e02e201d282bfab3eb9c9f1c8e39aba51fdac227
520MianXiangDuiXiang520 <15364968962@163.com>
2021-01-15 21:45:29 +0800

    :alembic:实验HTTP缓存

--- src/middleware/cache.go
+++ src/middleware/cache.go
@@ -6,6 +6,6 @@ import (
 
 func CacheMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		context.Header("Cache-Control", "no-cache,public,max-age=600")
+		context.Header("Cache-Control", "public,max-age=3600")
 	}
 }
